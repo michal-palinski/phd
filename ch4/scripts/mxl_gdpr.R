cc4b108023ca0524f0fb98eba0c82de2c2b56b9a
michal-palinski <m.palinski@uw.edu.pl>
2025-04-01 14:28:50 +0200

    Add files via upload

---
+++ ch4/scripts/mxl_gdpr.R
@@ -0,0 +1,190 @@
+# ################################################################# #
+#### LOAD LIBRARY AND DEFINE CORE SETTINGS                       ####
+# ################################################################# #
+
+### Clear memory
+rm(list = ls())
+
+### Load Apollo library
+library(apollo)
+
+### Initialise code
+apollo_initialise()
+
+### Set core controls
+apollo_control = list(
+  modelName ="NCN_mxl_gdpr",
+  modelDescr ="Mixed logit model on privacy choices - gdpr",
+  indivID   ="TOKEN",  
+  mixing    = TRUE, 
+  nCores    = 4
+)
+
+# ################################################################# #
+#### LOAD DATA AND APPLY ANY TRANSFORMATIONS                     ####
+# ################################################################# #
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+database = read.csv("../data/in.csv",header=TRUE)
+database = subset(database, `GROUP.1` == 'Z RODO')
+
+# ################################################################# #
+#### DEFINE MODEL PARAMETERS                                     ####
+# ################################################################# #
+
+### Vector of parameters, including any that are kept fixed in estimation
+apollo_beta = c(
+  # asc_mu = 0,
+  # asc_sigma =1,
+  
+  pd.name_mu = 0,
+  pd.name_sigma = .1,
+  pd.birth_mu = 0,
+  pd.birth_sigma = .1,
+  pd.sm_mu = 0,
+  pd.sm_sigma = .1,
+  pd.search_mu = 0,
+  pd.search_sigma = .1,
+  
+  ld.route_mu = 0,
+  ld.route_sigma = .1,
+  ld.hist_mu = 0,
+  ld.hist_sigma = .1,
+  
+  cd.email_mu = 0,
+  cd.email_sigma = .1,
+  cd.tel_mu = 0,
+  cd.tel_sigma = .1,
+  
+  ua.app_mu = 0,
+  ua.app_sigma = .1,
+  ua.ad_mu = 0,
+  ua.ad_sigma = .1,
+  ua.surv_mu = 0,
+  ua.surv_sigma = .1,
+  
+  discount_mu = 0,
+  discount_sigma =.1
+)
+
+### Vector with names (in quotes) of parameters to be kept fixed at their starting value in apollo_beta, use apollo_beta_fixed = c() if none
+apollo_fixed = c()
+
+# ################################################################# #
+#### DEFINE RANDOM COMPONENTS                                    ####
+# ################################################################# #
+
+### Set parameters for generating draws
+apollo_draws = list(
+  interDrawsType = "sobolOwen",
+  interNDraws    = 1000,
+  interUnifDraws = c(),
+  interNormDraws = c("draws_pd.name", "draws_pd.birth", "draws_pd.sm", "draws_pd.search", "draws_ld.route", "draws_ld.hist", "draws_cd.email", "draws_cd.tel", "draws_ua.app", "draws_ua.ad", "draws_ua.surv", "draws_discount"), 
+  # "draws_asc"),
+  intraDrawsType = "halton",
+  intraNDraws    = 0,
+  intraUnifDraws = c(),
+  intraNormDraws = c()
+)
+
+### Create random parameters
+apollo_randCoeff = function(apollo_beta, apollo_inputs){
+  randcoeff = list()
+  
+  # randcoeff[["asc"]] = asc_mu + asc_sigma*draws_asc
+  
+  randcoeff[["pd.name"]] = pd.name_mu + pd.name_sigma * draws_pd.name
+  randcoeff[["pd.birth"]] = pd.birth_mu + pd.birth_sigma * draws_pd.birth
+  randcoeff[["pd.sm"]] = pd.sm_mu + pd.sm_sigma * draws_pd.sm
+  randcoeff[["pd.search"]] = pd.search_mu + pd.search_sigma * draws_pd.search
+  
+  randcoeff[["ld.route"]] = ld.route_mu + ld.route_sigma * draws_ld.route
+  randcoeff[["ld.hist"]] = ld.hist_mu + ld.hist_sigma * draws_ld.hist
+  
+  randcoeff[["cd.email"]] = cd.email_mu + cd.email_sigma * draws_cd.email
+  randcoeff[["cd.tel"]] = cd.tel_mu + cd.tel_sigma * draws_cd.tel
+  
+  randcoeff[["ua.app"]] = ua.app_mu + ua.app_sigma * draws_ua.app
+  randcoeff[["ua.ad"]] = ua.ad_mu + ua.ad_sigma * draws_ua.ad
+  randcoeff[["ua.surv"]] = ua.surv_mu + ua.surv_sigma * draws_ua.surv
+  
+  randcoeff[["discount"]] = exp(discount_mu + discount_sigma*draws_discount)
+  
+  return(randcoeff)
+}
+
+# ################################################################# #
+#### GROUP AND VALIDATE INPUTS                                   ####
+# ################################################################# #
+
+apollo_inputs = apollo_validateInputs()
+
+# ################################################################# #
+#### DEFINE MODEL AND LIKELIHOOD FUNCTION                        ####
+# ################################################################# #
+
+apollo_probabilities=function(apollo_beta, apollo_inputs, functionality="estimate"){
+  
+  ### Function initialisation: do not change the following three commands
+  ### Attach inputs and detach after function exit
+  apollo_attach(apollo_beta, apollo_inputs)
+  on.exit(apollo_detach(apollo_beta, apollo_inputs))
+  
+  ### Create list of probabilities P
+  P = list()
+  
+  ### List of utilities: these must use the same names as in mnl_settings, order is irrelevant
+  V = list()
+  
+  V[['alt1']]  =  alt1.a_1*pd.name + alt1.a_2*pd.birth + alt1.a_3*pd.sm + alt1.a_4*pd.search + alt1.b_1*ld.route + alt1.b_2*ld.hist + alt1.c_1*cd.email + alt1.c_2*cd.tel + alt1.d_1*ua.app + alt1.d_2*ua.ad + alt1.d_3*ua.surv + alt1.discount*discount
+  V[['alt2']]  =  alt2.a_1*pd.name + alt2.a_2*pd.birth + alt2.a_3*pd.sm + alt2.a_4*pd.search + alt2.b_1*ld.route + alt2.b_2*ld.hist + alt2.c_1*cd.email + alt2.c_2*cd.tel + alt2.d_1*ua.app + alt2.d_2*ua.ad + alt2.d_3*ua.surv + alt2.discount*discount
+  V[['alt3']]  =  alt3.a_1*pd.name + alt3.a_2*pd.birth + alt3.a_3*pd.sm + alt3.a_4*pd.search + alt3.b_1*ld.route + alt3.b_2*ld.hist + alt3.c_1*cd.email + alt3.c_2*cd.tel + alt3.d_1*ua.app + alt3.d_2*ua.ad + alt3.d_3*ua.surv + alt3.discount*discount
+  V[['alt4']]  =  alt4.a_1*pd.name + alt4.a_2*pd.birth + alt4.a_3*pd.sm + alt4.a_4*pd.search + alt4.b_1*ld.route + alt4.b_2*ld.hist + alt4.c_1*cd.email + alt4.c_2*cd.tel + alt4.d_1*ua.app + alt4.d_2*ua.ad + alt4.d_3*ua.surv + alt4.discount*discount
+  # V[['alt4']]  =  asc*asc.4
+  
+  
+  ### Define settings for MNL model component
+  mnl_settings = list(
+    alternatives  = c(alt1=1, alt2=2, alt3=3, alt4=4),
+    avail         = list(alt1=1, alt2=1, alt3=1, alt4=1),
+    choiceVar     = choice,
+    V             = V
+  )
+  
+  ### Compute probabilities using MNL model
+  P[['model']] = apollo_mnl(mnl_settings, functionality)
+  
+  ### Take product across observation for same individual
+  P = apollo_panelProd(P, apollo_inputs, functionality)
+  
+  ### Average across inter-individual draws
+  P = apollo_avgInterDraws(P, apollo_inputs, functionality)
+  
+  ### Prepare and return outputs of function
+  P = apollo_prepareProb(P, apollo_inputs, functionality)
+  return(P)
+}
+
+# ################################################################# #
+#### MODEL ESTIMATION                                            ####
+# ################################################################# #
+
+model = apollo_estimate(apollo_beta, apollo_fixed,
+                        apollo_probabilities, apollo_inputs, 
+                        estimate_settings=list())
+
+# ################################################################# #
+#### MODEL OUTPUTS                                               ####
+# ################################################################# #
+
+# ----------------------------------------------------------------- #
+#---- FORMATTED OUTPUT (TO SCREEN)                               ----
+# ----------------------------------------------------------------- #
+
+apollo_modelOutput(model)
+
+# ----------------------------------------------------------------- #
+#---- FORMATTED OUTPUT (TO FILE, using model name)               ----
+# ----------------------------------------------------------------- #
+
+apollo_saveOutput(model)
+
---
+++ ch5/scripts/netflix_s2.m
@@ -0,0 +1,157 @@
+% The script requires:
+% https://github.com/czaj/dce
+% https://github.com/czaj/tools
+% (they need to be downloaded and included in Matlab paths (with subfolders))
+
+clear all
+clc
+global B_backup; % this is for storing B (parameters) vector in case iterations are interrupted with ctrl-c
+
+%% ****************************  loading data  ****************************
+EstimOpt.DataFile = ('netflix_s2.mat'); % specify the name of the data file
+
+DATA = load(EstimOpt.DataFile); % load data, store it in a structure DATA
+
+%% ****************************  data description  ****************************
+%tableDCE = [DATA.RID, DATA.index, DATA.SCENARIO, DATA.block, DATA.alt, DATA.choice, DATA.x1, DATA.x2, DATA.x3, DATA.x4, DATA.x5, DATA.x6, DATA.x7, DATA.x8,];
+%tableDCE = array2table(tableDCE, 'VariableNames', {'RID','Id', 'Scenario', 'Block' 'Alt', 'Choice', 'Ratings','History','AgeGender','Location','ExternalData','Ads','AdsTime','Discount'});
+
+%% ****************************  model specification  ****************************
+
+EstimOpt.ProjectName = 'Netflix'; % name of the project used for naming xls results files
+
+DATA.Y = DATA.choice; % explained variable (choice indicator)
+
+DATA.Xa = [DATA.alt==3, DATA.x3, DATA.x4, DATA.x5, (DATA.x6/10)]; 
+
+% DATA.Xa = [DATA.x3, DATA.x4, DATA.x5, (DATA.x6/10).*(DATA.alt<3)]; 
+EstimOpt.NamesA = {'ASC3';'AgeGender';'Location';'ExternalData'; 'SearchTime'};
+
+DATA.treatment = logical(DATA.treatment2_attentive);
+
+DATA.Xm = [DATA.treatment2_attentive];
+EstimOpt.NamesM = {'DisplayTreatment'};
+
+%% ****************************  specifying input ****************************
+
+% DATA.filter = ones(size(DATA.treatment)) == 1; % modify to include only some observations, e.g. model for male respondents only
+INPUT.Y = DATA.Y; 
+INPUT.Xa = DATA.Xa;
+INPUT.Xm = DATA.Xm;
+% INPUT.Xm = DATA.Xm(DATA.filter,:); % Explanatory variables of random parameters means / inreactions
+% INPUT.Xs = DATA.Xs(DATA.filter,:); % Explanatory variables of scale
+% INPUT.Xt = DATA.Xt(DATA.filter,:); % Explanatory variables of scale variance (GMXL model)
+% INPUT.Xc = DATA.Xc(DATA.filter,:); % Explanatory variables of class membership (Latent class models)
+% INPUT.Xmea = DATA.Xmea(DATA.filter,:); % Measurment equations variables (Hybrid models)
+% INPUT.Xmea_exp = DATA.Xmea_exp(DATA.filter,:); % Additional covariates explaining mesurment equations (Hybrid models)
+% INPUT.Xstr = [DATA.female,DATA.city,DATA.high_edu];
+% INPUT.Xstr = DATA.Xstr(DATA.filter,:); % Structural equations variables (Hybrid models)
+% INPUT.Xstr = [DATA.female,DATA.city,DATA.high_edu,DATA.treatment];
+% EstimOpt.NamesStr = {'female','city','high_edu','display_treatment'};
+% INPUT.MissingInd = DATA.SKIP(DATA.filter,:); % use to indicate missing observations, e.g. choice tasks with no answer
+
+
+%% ****************************  sample characteristics ****************************
+
+EstimOpt.NCT = 8; % Number of choice tasks per person 
+EstimOpt.NAlt = 3; % Number of alternatives
+EstimOpt.NP = length(INPUT.Y)/EstimOpt.NCT/EstimOpt.NAlt; % Number of respondents
+
+%% **************************** estimation and optimization options ****************************
+
+EstimOpt.eps = 1.e-9; % overall precision level
+
+[INPUT, Results, EstimOpt, OptimOpt] = DataCleanDCE(INPUT,EstimOpt);
+
+EstimOpt.NRep = 1e3; % number of draws for numerical simulation
+% OptimOpt.MaxIter = 1e3; % maximum number of iterations
+% OptimOpt.Algorithm = 'trust-region'; % 'quasi-newton'
+% EstimOpt.NumGrad = 1; % 0
+% OptimOpt.GradObj = 'off'; % 'off'
+% OptimOpt.FinDiffType = 'central'; % 'forward'
+% OptimOpt.Hessian = 'user-supplied'; % 'off'
+% EstimOpt.HessEstFix = 1s; % 0 = use optimization Hessian, 1 = use jacobian-based (BHHH) Hessian, 2 - use high-precision jacobian-based (BHHH) Hessian 3 - use numerical Hessian
+% EstimOpt.ApproxHess = 0;
+
+% Estimopt.RealMin = 1; % use in the case of numerical errors (possibly for a few iterations only)
+
+
+%% ****************************     MNL     ****************************
+
+EstimOpt.WTP_space = 0; % number of monetary attributes for WTP-space estimation (need to come last in Xa)
+% EstimOpt.WTP_matrix = []; % specify which monetary parameter is used for which non-monetary attribute for WTP-space models
+% EstimOpt.NLTType = 1 % 1 = Box-Cox transformations; 2 = Yeo-Johnson transofmarions (MNL and MXL only)
+% EstimOpt.NLTVariables = []; % choose Xa to be non-linearly transformed (MNL and MXL only)
+
+% Optional: pass starting values either using B_backup (global variable automatically saved at each iteration) or Results.MNL.b0
+% B_backup = [0.074;0.11;-0.1;-0.033;-0.066;-0.14;0.27;-0.029;0.1;-0.012;0.044;0.12;-0.003;0.049;-0.025;0.049;-0.00054;-0.053];
+OptimOpt.Algorithm = 'trust-region'; % 'quasi-newton';
+OptimOpt.Hessian = 'user-supplied'; % 'off'
+
+% OptimOpt.Algorithm = 'quasi-newton';
+% OptimOpt.Hessian = 'off';
+
+Results.MNL = MNL(INPUT,Results,EstimOpt,OptimOpt);
+
+%% ****************************     MXL_d     ****************************
+
+% EstimOpt.WTP_space = 1; % number of monetary attributes for WTP-space estimation (need to come last in Xa)
+% EstimOpt.FullCov = 0;
+EstimOpt.Dist = [0;0;0;0;1]; % optional: AdsTime lognormal
+
+% B_backup = [-2.79907114487808,
+% -1.41908651026310,
+% -2.24979173675302,
+% -2.84423140264558,
+% -8.91608328388209,
+% 6.06197601256620,
+% 1.52207695015508,
+% 1.70655006952016,
+% 2.12093978149790,
+% 7.28251659755717,
+% -0.724328043646419,
+% -0.374520836130271,
+% -0.236288638854712,
+% -0.303515246360755,
+% -0.604956930112001];
+
+Results.MXL = MXL(INPUT,Results,EstimOpt,OptimOpt);
+
+%% ****************************     LC     ****************************
+EstimOpt.NClass = 3; % default
+EstimOpt.ClassScores = 1; % default (yes)
+% EstimOpt.BActiveClass = ones(EstimOpt.NVarA,1); % default Specify which coefficients should be kept constant (=0)
+Results.LC = LC(INPUT,Results,EstimOpt,OptimOpt);
+
+% LCMXL_d: Latent Class Mixed Logit Model without correlated random parameters
+EstimOpt.FullCov = 0;
+% Results.LCMXL_d = LCMXL(INPUT,Results,EstimOpt,OptimOpt);
+
+%% ****************************     HMXL_d     ****************************
+% EstimOpt.NLatent = 2;
+% INPUT.Xmea = [DATA.q16_1,DATA.q16_2,DATA.q16_3,DATA.q16_4,DATA.q262_1,DATA.q262_2,DATA.q262_3,DATA.q262_4];
+% EstimOpt.NamesMea = {'ads1','ads2','ads3','ads4','net1','net2','net3','net4'};
+% 
+% EstimOpt.MeaMatrix = [1 1 1 1 0 0 0 0  ; ...
+%                       0 0 0 0 1 1 1 1  ; ...
+%                       ];
+% 
+% EstimOpt.MeaSpecMatrix = [2];
+% 
+% EstimOpt.FullCov = 0;
+% 
+% % B_backup = [-1.1863936151292135968;0.8100159302560846486;-1.4761390114132448836;0.74339536228005975715;2.4048971350289942706;1.5808751669470322287;0.48723630726156613724;1.2923693853728603909;0;0;0.94457242872305691161;0;0;0;-0.60910613192101137336;0;0;0;-0.04938522467448652753;0;0;0;-0.34514206638887207079;0;0;0;0.06137634855341076584;0;0;0;-0.77679094314073415806;0;0;0;-0.46730941911909723574;0;0;0;0.59701345726712296003;0;-0.18516360231222792065;-1.1719835422058162422;-0.37019459087453671087;-0.57193879456064045108;0.10736601055416811201;-1.2127340313551224771;-0.79695205082660847129;0.13958425315693717694;-0.4489985994625657062;0.40734657267637469635;-0.41520736227722615519;-2.3602505157295796678;0.09820592992580901015;-0.14636090020784695009;0.21161950850843774807;-0.15307156629551596505;-1.1439531568136254158;-0.10932776505693950209;-0.274710725674727263;-0.11050680043783617235;-0.014118535791687595454;-2.070026098153683769;0.017919944140045624553;-0.35128344796499366698;0.13469622335087202969;0.0076364454490646460974;-1.9950625318931582974;-0.21080195833136447514;-0.28011130394120098419;0.11830580404877050205;0.80519613725390182246;-2.7166025070747448211;-0.011441775399824948706;0.18826295356999717123;0.52464879645123962959;-0.14749169623220997893;-1.7131662206723403674;-0.36049138191155150057;-0.4972630915582898492;0.010576005871886779222];
+% % EstimOpt.BActive = ones(size(B_backup));
+% % EstimOpt.BActive([9,10,12, 13,14,16, 17,18,20, 21,22,24, 25,26,28, 29,30,32, 33,34,36, 37,38,40]) = 0;
+% % B_backup = B_backup .* EstimOpt.BActive;
+% 
+% EstimOpt.RealMin = 1;
+% 
+% OptimOpt.Algorithm = 'trust-region'; % 'quasi-newton';
+% OptimOpt.Hessian = 'user-supplied'; % 'off'
+% 
+% EstimOpt.NRep = 1e3; % number of draws for numerical simulation
+% % Results.HMNL = HMNL(INPUT,Results,EstimOpt,OptimOpt);
+% 
+% EstimOpt.NRep = 5e3; % number of draws for numerical simulation
+% % Results.HMXL = HMXL(INPUT,Results,EstimOpt,OptimOpt);
